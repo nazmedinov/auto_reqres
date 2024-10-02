1c9ffff93e804f5f6d78b08bcb377a27925a55a8
nazmedinov <nazmedinov.adel@yandex.ru>
2024-10-02 23:41:46 +0300

    AU-5 add several ui tests

--- conftest.py
+++ conftest.py
@@ -1,10 +1,24 @@
+import allure
 import pytest
 
 from api_collection.users.api_users import UsersAPI
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+@allure.step('Подготовка тестового юзера')
 @pytest.fixture
 def new_user_fixture():
     users_api = UsersAPI()
     user = users_api.create_user()
     yield user
     users_api.delete_user(user.id)
+
+@allure.step('Открытие браузера')
+@pytest.fixture
+def browser():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(chrome_options)
+    browser.implicitly_wait(3)
+
+    return browser
---
+++ ui_tests/test_main_page.py
@@ -0,0 +1,24 @@
+import allure
+from pages_collection.main_page import MainPage, MainPageLocators
+from pages_collection.config import PagesURL
+
+@allure.epic('Client Pages')
+@allure.feature('Main Page')
+class TestMainPage:
+
+    @allure.title('Проверка открытия главной страницы')
+    def test_open_main_page(self, browser):
+        main_page = MainPage(browser)
+        with allure.step('Главная страница успешна открыта'):
+            main_page.open()
+            main_page.wait_for_visibility(MainPageLocators.MAIN_PAGE_TITLE)
+
+    @allure.title('Проверка перехода к получению сертификата с главной страницы')
+    def test_go_to_online_training(self, browser):
+        main_page = MainPage(browser)
+        main_page.open()
+        main_page.wait_and_click(MainPageLocators.ONLINE_TRAINING_BUTTON)
+        main_page.switch_to_window(window_number=1)
+        with allure.step('Успешный переход на страницу получения сертификата'):
+            main_page.wait_for_visibility(MainPageLocators.TRAINING_PAGE_TITLE)
+            main_page.check_current_url(PagesURL.CERTIFICATE_PAGE_URL)
